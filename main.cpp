2a3dba6abeb5b02b59b151a6299a6f78bb90e40a
SrMilton <35127424+SrMilton@users.noreply.github.com>
2021-01-19 23:02:29 -0300

    .mp4 support + bug fixes

--- main.cpp
+++ main.cpp
@@ -3,14 +3,15 @@
 int main()
 {
 
-	cout << "File name(without the extension): ";
+	cout << "File name or path: ";
 	cin >> filename;
+	filename.erase(remove(filename.begin(), filename.end(), '"'), filename.end());//Removing "
+	cout << filename;
 
-	//filename = filename + ".webm";
 
-	if (!extension(filename))
+	if (strcmp(extension(filename).c_str(), "webm")==0 && strcmp(extension(filename).c_str(), "mp4")==0)
 	{
-		cout << "Please try again with a .webm video (You can use a online video converter)." << endl;
+		cout << "Please try again with a .webm/.mp4 video (You can use a online video converter)." << endl;
 		system("pause");
 		system("cls");
 		main();
@@ -22,24 +23,39 @@ int main()
 	{
 		while (!binaryFile.eof()) 
 		{
-			
-			if (search(input_iter_t(binaryFile), input_iter_t(), search_001, search_001 + SIZE_001) != input_iter_t())
+			if (!strcmp(extension(filename).c_str(), "webm")) //Converter WEBM
 			{
-				cout << "Address Found!" << endl;
-				cout << "Applying exploit.." << endl;
-				binaryFile.seekp(-SIZE_001, ios::cur);
-				binaryFile.write(reinterpret_cast<const char*>(replace_001), SIZE_002);
-				cout << "Sucessfull!" << endl;
-				break;
+				if (search(input_iter_t(binaryFile), input_iter_t(), search_001, search_001 + SIZE_001) != input_iter_t())
+				{
+					cout << "Address Found!" << endl;
+						cout << "Applying exploit.." << endl;
+						binaryFile.seekp(-SIZE_001, ios::cur);
+						binaryFile.write(reinterpret_cast<const char*>(replace_001), SIZE_002);
+						cout << "Sucessfull!" << endl;
+						break;
+				}
+				else
+				{
+					cout << "Address not find. Make sure your file is not corrupted or try convert to .webm again." << endl;
+				}
 			}
-			else
+			else if (!strcmp(extension(filename).c_str(), "mp4")) //Converter MP4
 			{
-				cout << "Address not find. Make sure your file is not corrupted or try convert to .webm again." << endl;
+				if (search(input_iter_t(binaryFile), input_iter_t(), searchmp4_001, searchmp4_001 + SIZEMP4_001) != input_iter_t())
+				{
+					cout << "Address Found!" << endl;
+					cout << "Applying exploit.." << endl;
+					binaryFile.seekp(+11, ios::cur);
+					binaryFile.write(reinterpret_cast<const char*>(replacemp4_001), SIZEMP4_002);
+					cout << "Sucessfull!" << endl;
+					break;
+				}
+				else
+				{
+					cout << "Address not find. Make sure your file is not corrupted or try convert to .mp4 again." << endl;
+				}
 			}
 		}
-		binaryFile.clear();		
-		system("pause");
-		return 0;
 	}
 	else
 	{
@@ -47,17 +63,17 @@ int main()
 		system("pause");
 		return 0;
 	}
+		binaryFile.clear();		
+		system("pause");
+		return 0;
+
 	
 	return 0;
 }
 
-bool extension(string file_name)
+string extension(string file_name)
 {
 	int position = file_name.find_last_of(".");
 	string result = file_name.substr(position + 1);
-	if (strcmp(result.c_str(), "webm"))
-	{
-		return false;
-	}
-	else return true;
+	return result;
 }
--- main.h
+++ main.h
@@ -8,12 +8,19 @@
 
 using namespace std;
 
-bool extension(string file_name);
+string extension(string file_name);
 
+
+//========.webm hex==========
 const off_t SIZE_001 = 2;
 const off_t SIZE_002 = 7;
 unsigned char search_001[] = { 0x44, 0x89 };
 unsigned char replace_001[] = { 0x89,0x88,0x40,0xB0,0x7D,0xB0,0x00 };
+//========.mp4 hex==========
+const off_t SIZEMP4_001 = 4;
+const off_t SIZEMP4_002 = 8;
+unsigned char searchmp4_001[] = { 0x6D, 0x76, 0x68, 0x64 };
+unsigned char replacemp4_001[] = { 0x00,0x00,0x03,0xE8,0x00,0x00,0x13,0x88 };
 
 string filename;
 
